35a20d2049ac1f8b4ba2327626747b577db7b937
Raaynbo <rayan.boudjema24@gmail.com>
2024-09-16 00:23:55 +0200

    add - css+js: creating DOM element for the history page dynamically and created temp css rules for visualization

--- src/history.js
+++ src/history.js
@@ -2,18 +2,39 @@
 function historyPage(container){
 
 	// container avec img background carrousel + logo 
-	const hometxt = document.createElement('div');
-	const hoeetxt = document.createElement('div');
-	hometxt.classList.add("home_intro");
-	hoeetxt.classList.add("home_intro");
-	container.classList.add("history_layout")
+	const hdate = document.createElement('div');
+	const hgallery = document.createElement('div');
+	hdate.classList.add("history_date");
+	hgallery.classList.add("history_gallery");
+	container.classList.add("history_layout");
 	
-	hometxt.textContent = "this is a fantastic new page where we weill display a cool diaporama for every key moment of the restaurant in his historydsj aijdiaksdjakldajbc ibo js ihubyvugbiufosisoi hsocicsic s hnco scs inoc sicn socsnoc isdhcisdhcsi h sc ishc osihcssjkfhskf sdof if shdfosh fsidh foef hefj skfsh fkfrf soo oi sio sjdf hskjf hskd fhiwef qe qpw du dasiud aod hadai dofsodvvios chsid sh os hsiocso" 
-	hoeetxt.textContent = "this is a fantastic new page where we weill display a cool diaporama for every key moment of the restaurant in his historydsj aijdiaksdjakldajbc ibo js ihubyvugbiufosisoi hsocicsic s hnco scs inoc sicn socsnoc isdhcisdhcsi h sc ishc osihcssjkfhskf sdof if shdfosh fsidh foef hefj skfsh fkfrf soo oi sio sjdf hskjf hskd fhiwef qe qpw du dasiud aod hadai dofsodvvios chsid sh os hsiocso" 
-	
-	container.appendChild(hometxt);
-	container.appendChild(hoeetxt);
+	makeDateContent(hdate);
 
+	container.appendChild(hdate);
+	container.appendChild(hgallery);
+
+
+}
+
+function makeDateContent(container){
+	const carrousel = document.createElement('div');
+	const datebar = document.createElement('div');
+
+	const carrousel_img = document.createElement('img');
+	const carrousel_left = document.createElement('div'); 
+	const carrousel_right = document.createElement('div'); 
+
+	carrousel_img.classList.add("carrousel_img");
+	carrousel_right.classList.add("carrousel_right");
+	carrousel_left.classList.add("carrousel_left");
+	carrousel.classList.add("carrousel");
+	datebar.classList.add("datebar");
+
+	carrousel.appendChild(carrousel_img);
+	carrousel.appendChild(carrousel_left);
+	carrousel.appendChild(carrousel_right);
+	container.appendChild(carrousel);
+	container.appendChild(datebar);
 
 }
 
