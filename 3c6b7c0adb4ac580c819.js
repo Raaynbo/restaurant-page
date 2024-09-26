bd9fd2e9cc6fc4ee781c9a890f53e0390d85ee70
Raaynbo <rayan.boudjema24@gmail.com>
2024-09-26 15:25:39 +0200

    deployment init

---
+++ 3c6b7c0adb4ac580c819.js
@@ -0,0 +1,51 @@
+import "./style.css";
+import "./components.css";
+
+import {homePage} from "./home.js";
+import {historyPage} from "./history.js";
+import {locationPage} from "./location.js";
+import {socialPage} from "./socials.js";
+import {clearElement} from "./utils.js";
+console.log("restaurant-page");
+
+let actualContent = 0;
+
+
+const content = document.querySelector("#content");
+const homebtn = document.querySelector("#home");
+const historybtn = document.querySelector("#history");
+const menubtn = document.querySelector("#location");
+const aboutbtn = document.querySelector("#about");
+
+
+
+
+
+homebtn.addEventListener("click", (e) => {
+	clearElement(content, e.target);
+	actualContent = 1;
+	console.log(actualContent)
+	homePage(content);
+})
+historybtn.addEventListener("click", (e) => {
+	clearElement(content, e.target);
+	actualContent = 2;
+	console.log(actualContent)
+	historyPage(content);
+})
+menubtn.addEventListener("click", (e) => {
+	clearElement(content, e.target);
+	actualContent = 3;
+	console.log(actualContent)
+	locationPage(content);
+})
+//aboutbtn.addEventListener("click", (e) => {
+//	clearElement(content, e.target);
+//	actualContent = 4;
+//	console.log(actualContent)
+//	socialPage(content);
+//})
+homePage(content);
+
+
+export {actualContent};
